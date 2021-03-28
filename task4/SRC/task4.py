bda3dfed4777109da09c5a429dc33511fc0f3042
roman <33gra@mail.ru>
2021-03-28 14:19:05 +0300

    final version v4

--- task4/SRC/task4.py
+++ task4/SRC/task4.py
@@ -1,17 +1,21 @@
+import sys
+
 def find_index(input_str: str, request_str: str, beginning: int):
     ind = input_str.find(request_str, beginning)
     return ind + len(request_str) if ind != -1 else -1
-
 
 def are_similar(first_str: str, second_str: str):
     need_check_first_symbol = True if second_str and second_str[0] != '*' else False
     need_check_last_symbol = True if second_str and second_str[-1] != '*' else False
     substrings = second_str.split('*')
-    if len(substrings) == 1:
+    if second_str.find('*') == -1:
         return first_str == second_str
     substrings = [i for i in substrings if i]   # delete ''
+    if not substrings:
+        return True
     search_start_position = 0
-    for desired_substring in substrings:
+    substrings_ = substrings[:-1] if need_check_last_symbol else substrings
+    for desired_substring in substrings_:
         new_index = find_index(first_str, desired_substring, search_start_position)
         if new_index == -1:
             return False
@@ -20,18 +24,27 @@ def are_similar(first_str: str, second_str: str):
         else:
             return False
         need_check_first_symbol = False
-    if need_check_last_symbol == True and new_index-1 < len(first_str): return False
+    if need_check_last_symbol:
+        ind_ = first_str.rfind(substrings[-1])
+        if ind_ == -1:
+            return False
+        if ind_ + len(substrings[-1]) != len(first_str):
+            return False
     return True
 
+def main(first_str: str, second_str: str):
 
-def main():
-    first_str = input()
-    second_str = input()
+    #first_str = input()
+   # second_str = input()
     if are_similar(first_str, second_str):
         print("OK")
     else:
         print("KO")
 
-
 if __name__ == '__main__':
-    main()
+    args = sys.argv
+    if len(args) == 3:
+        main(args[1], args[2])
+    else:
+        print('KO')
+
